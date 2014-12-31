7d6b4b4fe4949f5714ae5b36da8d0638bfd5fabd
Nikos <nkatsikanis@gmail.com>
2014-12-31 13:11:30 +0000

    initial test with mocha

---
+++ test/datatypes/arithmetic.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var ubique = require('../../ubique');
+
+suite('datatypes', function () {
+	console.log('\nTesting datatypes/arrayfun ...');
+	test('arrayfun', function (done) {
+		assert.deepEqual(ubique.arrayfun([1.4, 2.3, 3], Math.log), [0.3364722366212129,0.8329091229351039,1.0986122886681098]);
+		done();
+	});
+});
+
+
+
+
+
+
