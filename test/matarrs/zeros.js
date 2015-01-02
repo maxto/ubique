c841463bda4c30ba77f30352413ec2106a70f90e
Nikos <nkatsikanis@gmail.com>
2015-01-02 11:19:07 +0000

    some matrix tests

---
+++ test/matarrs/zeros.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var ubique = require('../../ubique');
+
+suite('matarrs', function () {
+	console.log('\nTesting matarrs/matrix ...');
+	test('matrix', function (done) {
+
+		assert.equal(ubique.zeros(), 0);
+		assert.deepEqual(ubique.zeros(0), []);
+		assert.deepEqual(ubique.zeros(1), [[0]]);
+		assert.deepEqual(ubique.zeros(2), [[0, 0], [0, 0]]);
+		assert.deepEqual(ubique.zeros(2, 1), [[0], [0]]);
+		assert.deepEqual(ubique.zeros(1, 2), [[0, 0]]);
+		assert.deepEqual(ubique.zeros(2, 3), [[0, 0, 0], [0, 0, 0]]);
+
+		done();
+	});
+});
+
+
+
+
+
+
