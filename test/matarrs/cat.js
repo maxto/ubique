6df7c7148d1a6a22a724880de8e2cd1f66c76317
Massimiliano Todaro <m.todaro.ge@gmail.com>
2015-01-04 00:07:28 +0000

    test fix

--- test/matarrs/cat.js
+++ test/matarrs/cat.js
@@ -23,7 +23,7 @@ suite('matarrs', function () {
 		// Column concat
 		assert.deepEqual(ubique.cat(1, a, b), [[5, 6, 5, -1, 3, -1], [7, 8, -1, 4, 5, 9]]);
 		assert.deepEqual(ubique.cat(1, 5, 6, 7), [[5, 6, 7]]);
-		assert.deepEqual(ubique.cat(1,[[5,6,5],[7,8,-1]],[[-1,3,-1],[4,5,9]],[[3, 2], [5, 2]]), [[5, 6, 5, -1, 3, -1, 3, 2], [7, 8, -1, 4, 5, 9, 5, 2]]);
+		assert.deepEqual(ubique.cat(1,a,b,f), [[5, 6, 5, -1, 3, -1, 3, 2], [7, 8, -1, 4, 5, 9, 5, 2]]);
 
 		done();
 	});
